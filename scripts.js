6aeac5430f1a161903bba9b013667cc982ff993a
Bijirita <bijirita009@gmail.com>
2018-09-27 14:53:28 -0400

    commits

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
-var hero = ['hint', 'hstr', 'hcash']
-var bully = ['bint', 'bstr', 'bcash']
+//----------------------Global Variables----------------//
+
+var hero = [0, 0, 0]
+var bully = [1, 1, 1]
 var day = ["Monday", "Teusday", "Wednesday", "Thursday", "Friday"];
 var dayI = 0;
 var library = document.getElementById('Library');
@@ -10,15 +12,15 @@ var textbox = document.getElementById('text-box');
 var textp = document.querySelector('#text-box > p');
 var next = document.querySelector('#text-box > button');
 
+//------------------------Global Button Eventlistners----------------------//
 
 library.addEventListener('click', runLibrary);
+library.addEventListener('click', hero[0] ++);
 gym.addEventListener('click',runGym);
 work.addEventListener('click', runWork);
 fight.addEventListener('click', runFight);
 
-
-
-
+//----------------------Functions------------------------//
 function runLibrary () {
     console.log('checkpoint 1');
     textp.parentNode.removeChild(textp);
@@ -27,19 +29,38 @@ function runLibrary () {
     actionLibrary.textContent = "you go do the library thing"
     textbox.appendChild(actionLibrary);
     library.removeEventListener('click', runLibrary);
-    actionLibrary.appendChild(next);
-    next.addEventListener('click', proceedStory);
-    next.addEventListener('click', newDay);
+    textbox.appendChild(next);
+    next.addEventListener('click', proceedStoryLibrary);
+    // next.addEventListener('click', newDay);
+    console.log('checkpoint 3');
 }
 
-function proceedStory () {
+function runGym () {
+
+}
+
+function runWork () {
+
+}
+
+function runFight () {
+
+}
+
+
+function proceedStoryLibrary () {
     var textp = document.querySelector('#text-box > p');
     console.log('checkpoint 4');
     textbox.removeChild(textp);
     console.log('checkpoint 5');
-
-
+    var brainText = document.createElement('p');
+    brainText.textContent = "you feel your brain foldy fold";
+    textbox.appendChild(brainText);
+    textbox.appendChild(next);
+    next.onclick = newDay;
 }
+
+newDay();
 
 function newDay () {
     console.log(dayI);
@@ -47,3 +68,8 @@ function newDay () {
         var dayI = dayI[i];
     }
 }
+
+
+//if statement
+//incremetn counter
+//check if counter for day
