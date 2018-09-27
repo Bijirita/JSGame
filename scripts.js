b8b17d2c74c1d056ff2ec57c7386c655333305e3
Bijirita <bijirita009@gmail.com>
2018-09-27 12:44:39 -0400

    all buttons added

--- scripts.js
+++ scripts.js
@@ -6,9 +6,10 @@ var work = document.querySelector('#Work');
 var fight = document.querySelector('#Fight');
 var textbox = document.querySelector('#text-box');
 var textp = document.querySelector('#text-box > p');
+var next = document.querySelector('#text-box > button');
 
-
-library.onclick = test;
+// library.onclick = test;
+library.addEventListener('click', test);
 
 function test () {
     console.log('checkpoint,1');
@@ -17,4 +18,7 @@ function test () {
     var actionLibrary = document.createElement('p');
     actionLibrary.textContent = "you go do the library thing"
     textbox.appendChild(actionLibrary);
+    library.removeEventListener('click', test);
+    actionLibrary.appendChild(next);
+
 }
