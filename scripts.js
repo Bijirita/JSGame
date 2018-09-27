afeeb1c40cb07deef37595ec6767797bb9c3e347
Bijirita <bijirita009@gmail.com>
2018-09-27 13:31:32 -0400

    proceed story

--- scripts.js
+++ scripts.js
@@ -1,24 +1,33 @@
 var hero = ['hint', 'hstr', 'hcash']
 var bully = ['bint', 'bstr', 'bcash']
-var library = document.querySelector('#Library');
-var gym = document.querySelector('#Gym');
-var work = document.querySelector('#Work');
-var fight = document.querySelector('#Fight');
-var textbox = document.querySelector('#text-box');
+var library = document.getElementById('Library');
+var gym = document.getElementById('Gym');
+var work = document.getElementById('Work');
+var fight = document.getElementById('Fight');
+var textbox = document.getElementById('text-box');
 var textp = document.querySelector('#text-box > p');
 var next = document.querySelector('#text-box > button');
+var day = ["Monday", "Teusday", "Wednesday", "Thursday", "Friday"];
 
 // library.onclick = test;
 library.addEventListener('click', test);
 
 function test () {
-    console.log('checkpoint,1');
+    console.log('checkpoint 1');
     textp.parentNode.removeChild(textp);
-    console.log('checkpoint,2')
+    console.log('checkpoint 2')
     var actionLibrary = document.createElement('p');
     actionLibrary.textContent = "you go do the library thing"
     textbox.appendChild(actionLibrary);
     library.removeEventListener('click', test);
     actionLibrary.appendChild(next);
+    next.addEventListener('click', proceedStory);
+}
 
+
+function proceedStory () {
+        var textp = document.querySelector('#text-box > p');
+        console.log('checkpoint 4');
+        textbox.removeChild(textp);
+        console.log('checkpoint 5');
 }
