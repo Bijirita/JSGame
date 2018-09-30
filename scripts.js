e7ea96d18de13b827a6ae81fefc83fa84fb4751e
Nestor Peralta <bijirita009@gmail.com>
2018-09-29 23:00:45 -0400

    night commit

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ var hero = [0, 0, 0];
 var heroIndex = 0;
 var bully = [1, 1, 1];
 var storyIndex = 0;
-var storyText = ["You're a 4th grader chill'n at recess when that bully walks up to you.", "Bully: I'm kicking your ass on Friday.", "How do you Proceed?"];
+var storyText = ["You're a 4th grader chill'n at recess when that bully walks up to you.", "Bully: I'm kicking your ass on Friday.", "How do you respond?"];
 var dayIndex = 0;
 var dayText = ['How will you train']
 var day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
@@ -21,19 +21,15 @@ var next = document.getElementById('Next');
 
      function story () {
        
-        if (storyIndex <= 1) {
+        if (storyIndex < 1) {
             var next = document.getElementById('Next');
             var textp = document.querySelector('#text-box > p');
             storyIndex = ++storyIndex;
             textp.textContent = storyText[storyIndex];
-            console.log(storyIndex);
-            
-            console.log(storyIndex);
-            next.addEventListener('click', story); 
-            console.log('check1');
+            next.addEventListener('click', story);
         
-        }else if (storyIndex === 2) {
-            console.log('check2');
+        }else if (storyIndex >= 1) {
+            storyIndex = ++storyIndex;
             var textp = document.querySelector('#text-box > p');
             var next = document.getElementById('Next');
             var throwdown = document.createElement('button');
@@ -42,7 +38,7 @@ var next = document.getElementById('Next');
             textp.appendChild(throwdown);
             storyIndex = ++storyIndex;
             throwdown.addEventListener ('click', runFight);
-            next.textContent = 'See you Friday';
+            next.textContent = '"See you Friday."';
             next.addEventListener('click', newDay);
         }
     }
@@ -57,46 +53,48 @@ work.addEventListener('click', runWork);
 
 //----------------------Functions------------------------//
 function runLibrary () {
-    console.log('checkpoint 1');
     removeEar();
     hero[0] = hero[0] +=2;
-    var textbox = document.getElementById('text-box');
     var next = document.getElementById('Next');
     var textp = document.querySelector('#text-box > p');
-    console.log('checkpoint 2')
     textp.textContent = "you go do the library thing"
     next.style.display = "inline-block";
     next.textContent = "Next";
     next.addEventListener('click', newDay);
-    console.log('checkpoint 3');
 }
 
 function runGym () {
-    console.log(hero[1]);
+    removeEar();
     hero[1] = hero[1] +=2;
-    console.log(hero[1]);
+    var next = document.getElementById('Next');
+    var textp = document.querySelector('#text-box > p');
+    textp.textContent = "you go do the library thing"
+    next.style.display = "inline-block";
+    next.textContent = "Next";
+    next.addEventListener('click', newDay);
 }
 
 function runWork () {
-    console.log(hero[2]);
+    removeEar();
     hero[2] = hero[2] +=2;
-    console.log(hero[2]);
+    var next = document.getElementById('Next');
+    var textp = document.querySelector('#text-box > p');
+    textp.textContent = "you go do the library thing"
+    next.style.display = "inline-block";
+    next.textContent = "Next";
+    next.addEventListener('click', newDay)
 }
 
 function runFight () {
-    console.log('fight1');
+    bullySet ();
     for (var i = 0; heroIndex <= 2; i++) {
-        console.log('heroindex ' + heroIndex);
-        console.log('hero ' + hero);
         if (hero[heroIndex] > bully[heroIndex]) {
            return win();
         }else if (heroIndex === 2) {
            return lose();
         }else {
             heroIndex = ++heroIndex;
-            console.log(heroIndex);
         }
-        console.log(heroIndex);
     }
 }
 
@@ -116,9 +114,7 @@ function removeEar () {
 
 function proceedStoryLibrary () {
     var textp = document.querySelector('#text-box > p');
-    console.log('checkpoint 4');
     textbox.removeChild(textp);
-    console.log('checkpoint 5');
     var brainText = document.createElement('p');
     brainText.textContent = "you feel your brain foldy fold";
     textbox.appendChild(brainText);
@@ -129,22 +125,22 @@ function proceedStoryLibrary () {
 function win () {
     var textp = document.querySelector('#text-box > p');
     textp.textContent = 'You Win';
+    next.textContent = "Restart?";
 }
 
 function lose () {
     var textp = document.querySelector('#text-box > p');
     textp.textContent = 'You Lose';
+    next.textContent = "Restart?";
 }
 //---------------------New Day Functions------------------->
 
 function newDay () {
-    console.log('newday');
     var options = document.getElementById('interactions');
     options.style.display = "inline-block";
     if (dayIndex === day.length) {
         return runFight();
     } else if (dayIndex <= day.length - 1) {
-    console.log(dayIndex);
     var textp = document.querySelector('#text-box > p');
     var next = document.querySelector('#text-box > button');
     var library = document.getElementById('Library');
@@ -155,34 +151,16 @@ function newDay () {
     gym.addEventListener ('click', runGym);
     work.addEventListener ('click', runWork);
     fight.addEventListener ('click', runFight);
-    console.log("dayIndex " + dayIndex);
     next.style.display = "none";
     textp.textContent = 'Today is ' + day[dayIndex] + ' how will you spend your day?';
     dayIndex = ++dayIndex;
-    console.log('hero ' + hero);
     }
 }
-
-
-// var matTest = Math.floor(Math.random() * (2 - 0 + 1));
-// var bullyNum = Math.floor(Math.random() * 3);
-// console.log("check1 matTest is " + matTest);
-// console.log("check 2BNtest is " + bullyNum);
-
-bullySet();
 
 function bullySet () {
-    var bully = [1, 1, 1];
     var bullyIndex = Math.floor(Math.random() * 12) + 3;
-    console.log(bullyIndex);
     for (var i = 0; i < bullyIndex; i++) {
         var bArray = [Math.floor(Math.random() * 3)];
-        console.log("check bArray is " + bArray);
         bully[bArray] +=  1;
-        console.log(bully);
-        console.log("check4 bArray is now " + bArray); 
     }
-    
-
 }
-
